f9a7114dfa204406f3d0fe2aa9c9eb1dc10ca17f
natoy <espiritujhonny63@gmail.com>
2025-08-28 15:25:09 +0800

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Enter first number </label> <br>
+        <input type="text" name="num1"> <br>
+        <label for="">Enter Second Number</label> <br>
+        <input type="text" name="num2"> <br>
+        <label for="">Enter Third Number</label> <br>
+        <input type="text" name="num3"> <br>
+        <label for="">Enter Fourth Number</label> <br>
+        <input type="text" name="num4"> <br>
+        <input type="submit" value="calculate">
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
